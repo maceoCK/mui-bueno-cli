56da8efd4bd3633c8e3a34fac2ca56c85c4eedec
Maceo Cardinale Kwik <maceo.kwik@simoncomputing.com>
2025-06-25 11:57:58 -0400

    redo

--- dist/commands/download.d.ts
+++
@@ -1,3 +0,0 @@
-import { DownloadOptions } from '../types/index.js';
-export declare function downloadCommand(componentName?: string, options?: DownloadOptions): Promise<void>;
-//# sourceMappingURL=download.d.ts.map
